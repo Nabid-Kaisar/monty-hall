7a34f5d087be6b882a1c20b472b671b961013020
Nabid Kaisar <himelkaisar023@gmail.com>
2022-01-02 22:50:50 +0600

    partial calculation

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ import {Table, Row, Rows} from 'react-native-table-component';
 
 
 let userSelected = [];
+let userInitialSelection;
 const App: () => Node = () => {
     const [rewardDoor, setRewardDoor] = useState();
     const [doorText, setDoorText] = useState(['Door1', 'Door2', 'Door3']);
@@ -27,25 +28,30 @@ const App: () => Node = () => {
 
     const tableHead1 = ['NO.', 'WINNING']
     const tableHead2 = ['NO.', 'WINNING']
+    const [totalRow1, setTotalRow1] = useState(['TOTAL', 0]);
 
+    // const [tableData1, setTableData1] = useState([[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2], [1,2],[1,2],[1,2][1,2]]);
     const [tableData1, setTableData1] = useState([]);
     const [tableData2, setTableData2] = useState([]);
+    const [totalRow2, setTotalRow2] = useState(['TOTAL', 0]);
 
     useEffect(() => {
         setRewardDoor(randomRewardGenerator);
     }, []);
 
     useEffect(() => {
-        console.log(userSelected)
-        if(userSelected.length === 3){
-            //all done--> proceed to refresh
-            resetSimulation();
-        }
-    }, [doorText]);
+        calculatePercentage1();
+    }, [tableData1]);
+
+    useEffect(() => {
+        calculatePercentage2();
+    }, [tableData2]);
 
     const resetSimulation = () => {
         userSelected = [];
+        userInitialSelection = undefined;
         setRewardDoor(randomRewardGenerator);
+        console.log('rand')
         setDoorText(['Door1', 'Door2', 'Door3'])
     }
 
@@ -59,7 +65,7 @@ const App: () => Node = () => {
 
     //0,1,2
     const handleDoorClick = (event, doorNo: Number) => {
-        if(userSelected.includes(doorNo)){
+        if (userSelected.includes(doorNo) && doorNo !== userInitialSelection) {
             //already selected
             return;
         }
@@ -70,39 +76,119 @@ const App: () => Node = () => {
 
     const openDoor = (doorNo: Number) => {
         let oldDoorTextList: Array = [...doorText];
-        if (doorNo === rewardDoor) {
-            alert('You win a CAR!');
-            oldDoorTextList[doorNo] = 'CAR';
+
+        //userSelected already updated
+        //possible length 1, 2, 3
+        if (userSelected.length === 1 && doorNo === rewardDoor) {
+            // alert('You win a CAR!');
             resetSimulation();
+            return;
         } else {
             //monty hall scenario
             // oldDoorTextList[doorNo] = 'GOAT';
 
             //monty opens this door:
-            let wrongDoorToOpen: Number = [0, 1, 2].filter(d => d !== doorNo && d !== rewardDoor)[0];
+            if (userSelected.length === 1) {
+                userInitialSelection = doorNo;
+                let wrongDoorToOpen: Number = [0, 1, 2].filter(d => d !== doorNo && d !== rewardDoor)[0];
+                console.log('wrong', wrongDoorToOpen)
+                console.log('reward', rewardDoor)
+                //remaining two which might contain a reward
+                // let mightHaveRewardDoorList: Array = [];
+                // mightHaveRewardDoorList.push([0, 1, 2].filter(d => d !== wrongDoorToOpen));
 
-            //remaining two which might contain a reward
-            let mightHaveRewardDoorList: Array = [];
-            mightHaveRewardDoorList.push([0, 1, 2].filter(d => d !== wrongDoorToOpen));
+                oldDoorTextList[wrongDoorToOpen] = 'GOAT';
+                setDoorText(oldDoorTextList);
+            }
 
-            oldDoorTextList[wrongDoorToOpen] = 'GOAT';
+            //generate stats:
+            if (userSelected.length === 2) {
+                // console.log('userdoor', doorNo)
+                // console.log('rewardDoor', rewardDoor)
+                let win: Boolean;
+                if (rewardDoor === doorNo) {
+                    // alert("You win a CAR!");
+                    win = true;
+                } else {
+                    // alert("You get a GOAT LOL!")
+                    win = false;
+                }
+
+
+
+                if (userInitialSelection === doorNo) {
+                    let newTableData1 = [...tableData1];
+                    let no: Number = newTableData1.length + 1;
+                    newTableData1.push([no, win.toString()]);
+                    setTableData1(newTableData1)
+                } else {
+                    let newTableData2 = [...tableData2];
+                    let no: Number = newTableData2.length + 1;
+                    newTableData2.push([no, win.toString()]);
+                    setTableData2(newTableData2)
+                }
+
+                resetSimulation();
+                return;
+            }
+
         }
 
-        setDoorText(oldDoorTextList);
+
     };
 
+    const calculatePercentage1 = () => {
+        let values: [] = tableData1.map((t => {
+            if(t[1] == "true"){
+                return 1;
+            }else return 0;
+        }))
+
+        if (values.length === 0) {
+            setTotalRow1(['TOTAL', 0]);
+            return;
+        }
+
+        let sum = 0;
+        values.forEach((v) => {
+            sum += v;
+        })
+        setTotalRow1(['TOTAL',(( sum / values.length) * 100 + "%") ]);
+    }
+
+    const calculatePercentage2 = () => {
+
+        let values: [] = tableData2.map((t => {
+            if(t[1] == "true"){
+                return 1;
+            }else return 0;
+        }))
+
+        if (values.length === 0) {
+            setTotalRow1(['TOTAL', 0]);
+            return;
+        }
+
+        let sum = 0;
+        values.forEach((v) => {
+            sum += v;
+        })
+        setTotalRow2(['TOTAL',(( sum / values.length) * 100 + "%") ]);
+
+    }
+
     return (
-        <View style={{backgroundColor: 'black'}}>
+        <ScrollView style={{backgroundColor: 'black'}} contentContainerStyle={{flexGrow:1, height: 9000}}>
             <View style={styles.doorContainer}>
                 {/*<Text>{rewardDoor}</Text>*/}
                 <TouchableOpacity onPress={(event) => handleDoorClick(event, 0)} style={styles.door}>
-                    <Text>{doorText[0]}</Text>
+                    <Text style={styles.doorText}>{doorText[0]}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={(event) => handleDoorClick(event, 1)} style={styles.door}>
-                    <Text>{doorText[1]}</Text>
+                    <Text style={styles.doorText}>{doorText[1]}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={(event) => handleDoorClick(event, 2)} style={styles.door}>
-                    <Text>{doorText[2]}</Text>
+                    <Text style={styles.doorText}>{doorText[2]}</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.resetButton}>
@@ -116,18 +202,21 @@ const App: () => Node = () => {
             </View>
             <View>
                 <Text style={styles.tableHeader}>Re-selected Initial Door</Text>
-                <Table style={{marginBottom: 50, width: '80%', marginLeft: 40}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                <Table style={{marginBottom: 50, width: '80%', marginLeft: 40}}
+                       borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
                     <Row data={tableHead1} style={styles.head} textStyle={styles.text}/>
                     <Rows data={tableData1} textStyle={styles.text}/>
+                    <Row data={totalRow1} style={styles.head} textStyle={styles.text}/>
 
                 </Table>
                 <Text style={styles.tableHeader}>Change Selection</Text>
-                <Table style={{ width: '80%', marginLeft: 40}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                <Table style={{width: '80%', marginLeft: 40}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
                     <Row data={tableHead2} style={styles.head} textStyle={styles.text}/>
                     <Rows data={tableData2} textStyle={styles.text}/>
+                    <Row data={totalRow2} style={styles.head} textStyle={styles.text}/>
                 </Table>
             </View>
-        </View>
+        </ScrollView>
 
     );
 };
@@ -138,16 +227,18 @@ const styles = StyleSheet.create({
         borderColor: 'powderblue',
         borderWidth: 1,
         width: '20%',
-        height: '80%',
+        height: 150,
         marginRight: 5,
+        marginBottom: 15,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    tableHeader: {marginBottom: 5, textAlign: 'center',  justifyContent: 'center', alignItems: 'center'},
-    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-    head: { height: 40, backgroundColor: 'black' },
-    text: { margin: 6 },
-    resetButton: {width: '40%', height: '10%', marginLeft: 120}
+    tableHeader: {marginBottom: 5, textAlign: 'center', justifyContent: 'center', alignItems: 'center'},
+    container: {flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
+    head: {height: 40, backgroundColor: 'black'},
+    text: {margin: 6},
+    resetButton: {width: '40%',  marginLeft: 120, marginBottom: 20},
+    doorText: {color: 'yellow', fontWeight: '900', fontSize: 18}
 });
 
 export default App;
