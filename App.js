8354dc8cf46761587f1f8968b4ac50892cef5cfc
Nabid Kaisar <himelkaisar023@gmail.com>
2022-01-02 21:02:45 +0600

    reset functionality

--- App.js
+++ App.js
@@ -13,13 +13,17 @@ import {
     Text,
     View,
     TouchableOpacity,
-    ScrollView
+    ScrollView,
+    Button
 } from 'react-native';
 import {Table, Row, Rows} from 'react-native-table-component';
 
+
+let userSelected = [];
 const App: () => Node = () => {
     const [rewardDoor, setRewardDoor] = useState();
     const [doorText, setDoorText] = useState(['Door1', 'Door2', 'Door3']);
+
 
     const tableHead1 = ['NO.', 'WINNING']
     const tableHead2 = ['NO.', 'WINNING']
@@ -32,8 +36,18 @@ const App: () => Node = () => {
     }, []);
 
     useEffect(() => {
-        console.log(doorText)
+        console.log(userSelected)
+        if(userSelected.length === 3){
+            //all done--> proceed to refresh
+            resetSimulation();
+        }
     }, [doorText]);
+
+    const resetSimulation = () => {
+        userSelected = [];
+        setRewardDoor(randomRewardGenerator);
+        setDoorText(['Door1', 'Door2', 'Door3'])
+    }
 
     const randomRewardGenerator = () => {
         return getRandomInt(3);
@@ -45,20 +59,21 @@ const App: () => Node = () => {
 
     //0,1,2
     const handleDoorClick = (event, doorNo: Number) => {
+        if(userSelected.includes(doorNo)){
+            //already selected
+            return;
+        }
+
+        userSelected.push(doorNo);
         openDoor(doorNo);
-
     };
-
-    const openNotSelectedWrongDoor = (doorNo: Number) => {
-        let wrongDoorToOpen: Number = [0, 1, 2].filter(d => d !== doorNo && d !== rewardDoor)[0];
-        openDoor(wrongDoorToOpen);
-    }
 
     const openDoor = (doorNo: Number) => {
         let oldDoorTextList: Array = [...doorText];
         if (doorNo === rewardDoor) {
             alert('You win a CAR!');
             oldDoorTextList[doorNo] = 'CAR';
+            resetSimulation();
         } else {
             //monty hall scenario
             // oldDoorTextList[doorNo] = 'GOAT';
@@ -90,16 +105,24 @@ const App: () => Node = () => {
                     <Text>{doorText[2]}</Text>
                 </TouchableOpacity>
             </View>
-
+            <View style={styles.resetButton}>
+                <Button
+                    onPress={resetSimulation}
+                    title="Reset"
+                    color="red"
+                    accessibilityLabel="Click to RESET!"
+                    style={styles.resetButton}
+                />
+            </View>
             <View>
                 <Text style={styles.tableHeader}>Re-selected Initial Door</Text>
-                <Table style={{marginBottom: 50}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                <Table style={{marginBottom: 50, width: '80%', marginLeft: 40}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
                     <Row data={tableHead1} style={styles.head} textStyle={styles.text}/>
                     <Rows data={tableData1} textStyle={styles.text}/>
 
                 </Table>
                 <Text style={styles.tableHeader}>Change Selection</Text>
-                <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                <Table style={{ width: '80%', marginLeft: 40}} borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
                     <Row data={tableHead2} style={styles.head} textStyle={styles.text}/>
                     <Rows data={tableData2} textStyle={styles.text}/>
                 </Table>
@@ -123,7 +146,8 @@ const styles = StyleSheet.create({
     tableHeader: {marginBottom: 5, textAlign: 'center',  justifyContent: 'center', alignItems: 'center'},
     container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
     head: { height: 40, backgroundColor: 'black' },
-    text: { margin: 6 }
+    text: { margin: 6 },
+    resetButton: {width: '40%', height: '10%', marginLeft: 120}
 });
 
 export default App;
